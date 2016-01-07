e99778f1874e42df1bc49edba1a1be0e8ad8cf98
Frederic Junod <frederic.junod@camptocamp.com>
2016-01-07 09:47:33 +0100

    Update GitHub pages

---
+++ search_clear/examples/contribs/gmf/apps/mobile/js/mobile.js
@@ -0,0 +1,40 @@
+/**
+ * @fileoverview Application entry point.
+ *
+ * This file defines the "app_mobile" Closure namespace, which is be used as the
+ * Closure entry point (see "closure_entry_point" in the "build.json" file).
+ *
+ * This file includes `goog.require`'s for all the components/directives used
+ * by the HTML page and the controller to provide the configuration.
+ */
+goog.provide('app.MobileController');
+goog.provide('app_mobile');
+
+goog.require('app');
+goog.require('gmf.AbstractMobileController');
+/** @suppress {extraRequire} */
+goog.require('gmf.authenticationDirective');
+
+
+appModule.constant(
+    'authenticationBaseUrl',
+    'https://geomapfish-demo.camptocamp.net/2.0/wsgi');
+
+
+
+/**
+ * @param {ngeo.FeatureOverlayMgr} ngeoFeatureOverlayMgr The ngeo feature
+ *     overlay manager service.
+ * @param {Object} serverVars vars from GMF
+ * @constructor
+ * @extends {gmf.AbstractMobileController}
+ * @ngInject
+ * @export
+ */
+app.MobileController = function(ngeoFeatureOverlayMgr, serverVars) {
+  goog.base(this, ngeoFeatureOverlayMgr, serverVars);
+};
+goog.inherits(app.MobileController, gmf.AbstractMobileController);
+
+
+appModule.controller('MobileController', app.MobileController);
