9f6a72a08d0c4e1692c52222a04147c365e8c624
Travis <travis@travis-ci.org>
2016-02-22 16:13:56 +0000

    Update GitHub pages

---
+++ npm-install/examples/contribs/gmf/permalink.js
@@ -0,0 +1,39 @@
+
+
+
+/** @const **/
+var app = {};
+
+
+/** @type {!angular.Module} **/
+app.module = angular.module('app', ['gmf']);
+
+
+/**
+ * @constructor
+ */
+app.MainController = function() {
+
+  var projection = ol.proj.get('EPSG:21781');
+  projection.setExtent([485869.5728, 76443.1884, 837076.5648, 299941.7864]);
+
+  /**
+   * @type {ol.Map}
+   * @export
+   */
+  this.map = new ol.Map({
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      projection: projection,
+      resolutions: [200, 100, 50, 20, 10, 5, 2.5, 2, 1, 0.5],
+      center: [537635, 152640],
+      zoom: 3
+    })
+  });
+};
+
+app.module.controller('MainController', app.MainController);
