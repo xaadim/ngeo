5ecf47b5ae49c176891d3f12f7abc6f3722530a2
Travis <travis@travis-ci.org>
2016-03-01 16:20:23 +0000

    Update GitHub pages

--- master/examples/lib/ngeo-debug.js
+++ master/examples/lib/ngeo-debug.js
@@ -120813,6 +120813,43 @@ ngeo.interaction.MeasurePointMobile.prototype.handleMeasure = function(
   callback(output, coord);
 };
 
+goog.provide('ngeo.proj.EPSG2056');
+
+goog.require('ol.proj');
+
+if (typeof proj4 == 'function') {
+  proj4.defs('EPSG:2056',
+      '+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 ' +
+      '+k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel ' +
+      '+towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs');
+  ol.proj.get('EPSG:2056').setExtent([2420000, 1030000, 2900000, 1350000]);
+}
+
+goog.provide('ngeo.proj.EPSG21781');
+
+goog.require('ol.proj');
+
+if (typeof proj4 == 'function') {
+  var epsg21781def = [
+    '+proj=somerc',
+    '+lat_0=46.95240555555556',
+    '+lon_0=7.439583333333333',
+    '+k_0=1',
+    '+x_0=600000',
+    '+y_0=200000',
+    '+ellps=bessel',
+    '+towgs84=674.4,15.1,405.3,0,0,0,0',
+    '+units=m',
+    '+no_defs'
+  ].join(' ');
+
+  var extent = [420000, 30000, 900000, 350000];
+  proj4.defs('epsg:21781', epsg21781def);
+  proj4.defs('EPSG:21781', epsg21781def);
+  ol.proj.get('epsg:21781').setExtent(extent);
+  ol.proj.get('EPSG:21781').setExtent(extent);
+}
+
 goog.provide('ngeo.BackgroundEvent');
 goog.provide('ngeo.BackgroundEventType');
 goog.provide('ngeo.BackgroundLayerMgr');
