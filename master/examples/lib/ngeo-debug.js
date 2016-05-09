460f9b4b3443f335d2765d0fac08e4c483ecfa31
Travis <travis@travis-ci.org>
2016-05-09 07:36:23 +0000

    Update GitHub pages

--- master/examples/lib/ngeo-debug.js
+++ master/examples/lib/ngeo-debug.js
@@ -122430,14 +122430,33 @@ goog.provide('ngeo.filters');
 
 goog.require('ngeo');
 
-// format a number as a scale
-ngeo.module.filter('scalify', ['$filter', function(filter) {
-  var number = filter('number');
+/**
+ * Format a number as a localized scale.
+ * For instance:
+ *  - For 'fr-CH' the value 25000 will become '1 : 25 000'.
+ *  - For 'en-US' the value 25000 will become '1 : 25,000'.
+ *
+ * Example:
+ *
+ *      <p>{{25000 | ngeoScalify}}</p>
+ *
+ *
+ * @param {angular.$filter} $filter Angular filter
+ * @return {function(number): string} A function to format number into a 'scale'
+ *     string.
+ * @ngInject
+ * @ngdoc filter
+ * @ngname ngeoScalify
+ */
+ngeo.Scalify = function($filter) {
+  var number = $filter('number');
   return function(scale) {
-    var text = number(scale, 0) || '';
-    return '1 : ' + text.replace(/,/g, '\'');
+    var text = number(scale, 0);
+    return text ? '1 : ' + text : '';
   };
-}]);
+};
+
+ngeo.module.filter('ngeoScalify', ngeo.Scalify);
 
 goog.provide('ngeo.GetBrowserLanguage');
 
