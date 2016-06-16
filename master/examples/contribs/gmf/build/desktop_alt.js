9de2300d1c731595b3011651012b6ed1986b9f09
Travis <travis@travis-ci.org>
2016-06-16 09:11:59 +0000

    Update GitHub pages

--- master/examples/contribs/gmf/build/desktop_alt.js
+++ master/examples/contribs/gmf/build/desktop_alt.js
@@ -413,7 +413,7 @@ G=h("script,style"),u=e.extend({},A,q,k,v),z=h("background,cite,href,longdesc,sr
 //# sourceMappingURL=angular-sanitize.min.js.map
 
 /*
- AngularJS v1.5.6
+ AngularJS v1.5.7
  (c) 2010-2016 Google, Inc. http://angularjs.org
  License: MIT
 */
