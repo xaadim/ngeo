bce4ee61c5048a69427cedba49a0ecacaaf2f491
Travis <travis@travis-ci.org>
2016-06-22 07:09:06 +0000

    Update GitHub pages

--- master/examples/contribs/gmf/search.js
+++ master/examples/contribs/gmf/search.js
@@ -33,6 +33,21 @@ app.MainController = function(gmfThemes) {
     url: 'https://geomapfish-demo.camptocamp.net/2.1/wsgi/fulltextsearch'
   }];
 
+  var fill = new ol.style.Fill({color: [255, 255, 255, 0.6]});
+  var stroke = new ol.style.Stroke({color: [255, 0, 0, 1], width: 2});
+  /**
+   * @type {Object.<string, ol.style.Style>} Map of styles for search overlay.
+   * @export
+   */
+  this.searchStyles = {
+    'osm': new ol.style.Style({
+      fill: fill,
+      image: new ol.style.Circle({fill: fill, radius: 5, stroke: stroke}),
+      stroke: stroke
+    })
+  };
+
+
   /**
    * @type {ol.Map}
    * @export
